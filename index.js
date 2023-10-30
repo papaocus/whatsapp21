032f703c72cebf1963e10c7071d80d907ab36ba9
Danish Ansari <115074006+papaocus@users.noreply.github.com>
2023-10-30 15:01:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,45 +18,87 @@ app.post('/chatbot-reply', async (req, res) => {
 });
 
 const handleRequest2 = async (req, res) => {
-    const dataToSend = {
-        bot: "648701bbbf3af915b60daa2d",
-        sender: {
-            id: "6505d8ffbd59247f06e0ebaa",
-            name: "summer",
-            data: {}
-        },
-        message: {
-            text: req.body.payload.payload.text,
-            locale: ""
-        },
-        timestamp: req.body.timestamp
+    const payload = req.body.payload.payload;
+    const messagePayload = {
+        app: "DemoApp",
+        timestamp: req.body.timestamp,
+        version: 2,
+        type: "message",
+        payload: {
+            id: payload.id,
+            source: req.body.sender.id,
+            sender: {
+                phone: req.body.sender.id,
+                name: req.body.sender.name,
+                country_code: "91", // You can adjust this if needed
+                dial_code: "8x98xx21x4" // You can adjust this if needed
+            }
+        }
     };
 
+    if (payload.type === "image") {
+        messagePayload.payload.type = "image";
+        messagePayload.payload.payload = {
+            caption: payload.caption || "", // Optional caption
+            url: payload.url,
+            contentType: payload.contentType,
+            urlExpiry: payload.urlExpiry
+        };
+    } else if (payload.type === "audio") {
+        messagePayload.payload.type = "audio";
+        messagePayload.payload.payload = {
+            url: payload.url,
+            contentType: payload.contentType,
+            urlExpiry: payload.urlExpiry
+        };
+    } else if (payload.type === "video") {
+        messagePayload.payload.type = "video";
+        messagePayload.payload.payload = {
+            caption: payload.caption || "", // Optional caption
+            url: payload.url,
+            contentType: payload.contentType,
+            urlExpiry: payload.urlExpiry
+        };
+    } else if (payload.type === "document") {
+        messagePayload.payload.type = "file";
+        messagePayload.payload.payload = {
+            caption: payload.caption || "", // Optional caption
+            name: payload.name,
+            url: payload.url,
+            contentType: payload.contentType,
+            urlExpiry: payload.urlExpiry
+        };
+    } else if (payload.type === "sticker") {
+        messagePayload.payload.type = "sticker";
+        messagePayload.payload.payload = {
+            url: payload.url,
+            contentType: payload.contentType,
+            urlExpiry: payload.urlExpiry
+        };
+    } else {
+        // Handle other types or scenarios as needed
+    }
+
     try {
-        const response = await axios.post(API_URL, dataToSend, {
+        const response = await axios.post(API_URL, messagePayload, {
             headers: {
                 'x-api-key': API_KEY,
                 'content-type': 'application/json'
             }
         });
 
-        // console.log('please', response.data);
-
         // Await for the event to be emitted
         const answer = await new Promise(resolve => events.once('receivedChatbotReply', resolve));
         let parsedPayload = JSON.parse(answer.messagePayload);
         console.log('Received text:', parsedPayload.text);
 
-        // console.log('answer', answer.messagePayload.text);
         // Return the message from the chatbot-reply response
         return res.send(parsedPayload.text);
-
     } catch (error) {
-        console.error('Error calling the API 3:', error.response ? error.response.data : error.message);
+        console.error('Error calling the API:', error.response ? error.response.data : error.message);
         res.status(500).json({ status: 'error', message: 'Failed to call the API' });
     }
 };
-
 app.post('/callback', async (req, res) => {
     console.log('Received request from Gupshup:', req.body);
     await handleRequest2(req, res);
